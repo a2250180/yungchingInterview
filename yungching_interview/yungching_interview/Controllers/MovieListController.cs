24666eb4a18eff86d0eadfdbc3ead6c44d828505
黃宥勳 <u09900161@gmail.com>
2019-07-24 14:09:25 +0800

    Add Controller, View, DataAccess, BI

---
+++ yungching_interview/yungching_interview/Controllers/MovieListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace yungching_interview.Controllers
+{
+    public class MovieListController : Controller
+    {
+        // GET: MovieList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
